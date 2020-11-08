f620f4bdfde87c1c1e4a5845c801cb4121d2be08
Илья Дубов <dubovilya90@gmail.com>
2020-11-08 18:52:20 +0300

    added

---
+++ js/script.js
@@ -0,0 +1,38 @@
+Vue.use(VueAwesomeSwiper)
+
+new Vue({
+    el: '#vueapp',
+    data() {
+        return {
+            tabs1: true,
+            tabs2: false,
+            tabs3: false,
+            swiperOptionTop: {
+                loop: true,
+                loopedSlides: 5, 
+                spaceBetween: 10,
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev'
+                }
+            },
+            swiperOptionThumbs: {
+                loop: true,
+                loopedSlides: 5, 
+                spaceBetween: 10,
+                centeredSlides: true,
+                slidesPerView: 'auto',
+                touchRatio: 0.2,
+                slideToClickedSlide: true
+            }
+        }
+    },
+    mounted() {
+        this.$nextTick(() => {
+            const swiperTop = this.$refs.swiperTop.$swiper
+            const swiperThumbs = this.$refs.swiperThumbs.$swiper
+            swiperTop.controller.control = swiperThumbs
+            swiperThumbs.controller.control = swiperTop
+        })
+    }
+})
